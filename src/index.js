060fc784a2d36aab5378077b780f43ec41029842
Rahul Kumawat <rahullkumawat2527@gmail.com>
2026-01-15 19:18:38 +0530

    made utillities

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import dotenv from "dotenv"
-import connectDB  from "./db/index.js";
+import connectDB from "./db/index.js";
+import { app } from "./app.js"
 
 dotenv.config({
-    path : "./.env"
+    path: "./.env"
 })
 
+const PORT = process.env.PORT || 8000
+
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("error while connecting express to mongo db",error)
+            throw error
+        })
+        app.listen(PORT, () => {
+            console.log(`Server is running on port : ${PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("Mongo db connection failed !!! ", error)
+    })
 
